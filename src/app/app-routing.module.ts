b74dfde52bab50b0e9a1521c9761a2505048fc95
shashankpenumatcha <shashankpenumatcha@gmail.com>
2019-01-21 22:13:14 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutUsComponent } from './about us/aboutus.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AboutUsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
